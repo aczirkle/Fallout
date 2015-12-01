fbd3bf2e5fe50561604bb962a9ac53b7bc30c289
aczirkle <aczirkle@gmail.com>
2015-12-01 12:52:47 -0500

    Built sucsess

--- src/javafxapplication1/FXMLDocumentController.java
+++ src/javafxapplication1/FXMLDocumentController.java
@@ -20,17 +20,53 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
-    private TextArea strInput;
+    @FXML
+    private TextField strInput;
+    @FXML
+    private TextField perInput;
+    @FXML
+    private TextField endInput;
+    @FXML
+    private TextField chaInput;
+    @FXML
+    private TextField agiInput;
+    @FXML
+    private TextField intInput;
+    @FXML
+    private TextField lucInput;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
-    private void setStr(ActionEvent event){
-        strInput.setText("10");
-        
+    
+    @FXML
+    private void SaveCreate(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
     }
+    
+    @FXML
+    private void load(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @FXML
+    private void randomChar(ActionEvent event) {
+        //System.out.println("You clicked me!");
+        int x = (int) Math.random()*10;
+        strInput.setText("10");
+        perInput.setText(""+x);
+         endInput.setText("");
+        chaInput.setText("");
+         agiInput.setText("");      
+         intInput.setText("");
+        lucInput.setText("");
+
+    }
+
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
--- src/javafxapplication1/JavaFXApplication1.java
+++ src/javafxapplication1/JavaFXApplication1.java
@@ -24,6 +24,7 @@ public class JavaFXApplication1 extends Application {
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        //stage.setTitle("Fallout Character Maker");
         stage.show();
     }
 
