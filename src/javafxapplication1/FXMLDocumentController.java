c9458cc3f862175d4198b28ca7976638d5a597c7
aczirkle <aczirkle@gmail.com>
2015-12-01 12:29:38 -0500

    Added more to design

--- src/javafxapplication1/FXMLDocumentController.java
+++ src/javafxapplication1/FXMLDocumentController.java
@@ -10,7 +10,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 /**
  *
@@ -20,11 +20,16 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
+    private TextArea strInput;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
+    }
+    private void setStr(ActionEvent event){
+        strInput.setText("10");
+        
     }
     
     @Override
