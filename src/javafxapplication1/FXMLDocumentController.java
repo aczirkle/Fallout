c2a54882b3b40ea43ba14f385ebf7c50237b70fa
Damon Wolfgang Duckett <dduckett7537@gmail.com>
2015-12-01 19:28:01 -0500

    I made it so the character's skills are added when a character is created and added queries that get character information. The information from the queries are in ArrayLists and those are used in methods I added for you to use in displaying the information.

--- src/javafxapplication1/FXMLDocumentController.java
+++ src/javafxapplication1/FXMLDocumentController.java
@@ -17,6 +17,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 /**
  *
@@ -59,6 +60,15 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private TableColumn itemDescFull;
     
+    //TODO: Add in some comments and tell us what these are in the GUI please
+    /*Other thongs to do for the rest of us to do what we need to do:
+     *add in another tab for skills (could also be a character tab that shows all of a character's stats)
+     *add in something that allows us to give characters stuff in their inventory
+     *add in something that allows us to remove items from a character's inventory
+     *  make sure that it includes quantity of items added
+     *add in a item type thing to the inventory tab followed by a place to put in
+        the stat for that specific item type.*/
+    
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
@@ -90,7 +100,15 @@ public class FXMLDocumentController implements Initializable {
                + "," + cha + ");";
             st.executeUpdate(createString);
             
-            System.out.println("ti's done");
+            //later TODO: actually, take out the skills altogether since Fallout 4 has only perks (keep in for this though)
+            //Now add the character's skills to the database. Making them all 10 for now
+            
+            createString = "INSERT INTO Skills VALUES(10,10,10,10,10," + cha +");";
+            st.executeUpdate(createString);
+            
+            //Skills(Lockpicking int, Hacking int, Sneak int, Speech int, Science int, charID INTEGER NOT NULL PRIMARY KEY references Character(charID)
+
+            //System.out.println("ti's done");
        
        }
        catch(Exception e){
@@ -106,24 +124,61 @@ public class FXMLDocumentController implements Initializable {
     private void load(ActionEvent event) {
         //System.out.println("You clicked me!");
        // label.setText("Hello World!");
-       try{
-       createConnection();
-       Statement st = conn.createStatement();
-       ResultSet rs;
-      st.setQueryTimeout(30);  // set timeout to 30 sec.
-      rs = st.executeQuery("Select charID from Character where cName = '"+Name.getText()+"'");
-      st = conn.createStatement();
-       String cha=rs.getString("charID");
+    try{
+        createConnection();
+        Statement st = conn.createStatement();
+        ResultSet rs;
+        st.setQueryTimeout(30);  // set timeout to 30 sec.
+        rs = st.executeQuery("Select charID from Character where cName = '"+Name.getText()+"'");
+        st = conn.createStatement();
+        
+        //cha is the string of the charID and can be used in all the SQL statements
+        String cha = rs.getString("charID");
        
-      rs = st.executeQuery("Select * from special where charID = '"+cha+"'");
-      strInput.setText(Integer.toString(rs.getInt("strength")));
+        rs = st.executeQuery("Select * from special where charID = '" + cha + "'");
+        strInput.setText(Integer.toString(rs.getInt("strength")));
         perInput.setText(Integer.toString(rs.getInt("perception")));
-         endInput.setText(Integer.toString(rs.getInt("endurance")));
+        endInput.setText(Integer.toString(rs.getInt("endurance")));
         chaInput.setText(Integer.toString(rs.getInt("charisma")));
-         agiInput.setText(Integer.toString(rs.getInt("agility")));  
-         intInput.setText(Integer.toString(rs.getInt("intelligence")));
+        agiInput.setText(Integer.toString(rs.getInt("agility")));  
+        intInput.setText(Integer.toString(rs.getInt("intelligence")));
         lucInput.setText(Integer.toString(rs.getInt("luck")));
      //   System.out.println("Name = " + rs.getString("name"));
+     
+        //TODO: add in the stuff for getting all of the character's data to be used in other tabs
+        
+        //getting the requested character's perks
+        rs = st.executeQuery("SELECT perkName, perkInfo FROM Perks NATURAL JOIN HasPerks WHERE charID = " +
+                cha + " ;");
+        ArrayList<String> perkNames = new ArrayList<String>();
+        ArrayList<String> perkDescs = new ArrayList<String>();
+        
+        while(rs.next()) {
+            perkNames.add(rs.getString(1));
+            perkDescs.add(rs.getString(2));
+        }
+        
+        displayCharPerks(perkNames, perkDescs);
+        
+        //getting the requested character's inventory
+        ArrayList<String> itemNames = new ArrayList<String>();
+        ArrayList<String> itemQuants = new ArrayList<String>();
+        ArrayList<String> itemIDs = new ArrayList<String>();
+        rs = st.executeQuery("SELECT iID, iName, quantity FROM Items NATURAL JOIN HasItems\n" +
+            "WHERE charID = " + cha + " ;");
+        
+        while(rs.next()) {
+            itemIDs.add(rs.getString(1));
+            itemNames.add(rs.getString(2));
+            itemQuants.add(rs.getString(3));
+        }
+        /*now that we have the items a character has, it is time to figure out
+         *we are going to find out what kinf of item each item is */
+        /*TODO: to do this I am going to make three methods that check to see
+         *the iID of each returned item then adds them to an ArrayList that is
+         *returned. The returned ArrayList will then only consist of items of whatever
+         *type that was being tested.*/
+        
        }
        catch(Exception e){
            e.printStackTrace();
@@ -134,33 +189,39 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void randomChar(ActionEvent event) {
         //System.out.println("You clicked me!");
-       // double x = Math.random()*10;
+        // double x = Math.random()*10;
+        //TODO: make it so that the randomly generated values always add up to 35
         strInput.setText(Integer.toString((int) Math.ceil(Math.random()*10)));
         perInput.setText(Integer.toString((int) Math.ceil(Math.random()*10)));
-         endInput.setText(Integer.toString((int)Math.ceil(Math.random()*10)));
+        endInput.setText(Integer.toString((int)Math.ceil(Math.random()*10)));
         chaInput.setText(Integer.toString((int)Math.ceil(Math.random()*10)));
-         agiInput.setText(Integer.toString((int)Math.ceil(Math.random()*10)));  
-         intInput.setText(Integer.toString((int)Math.ceil(Math.random()*10)));
+        agiInput.setText(Integer.toString((int)Math.ceil(Math.random()*10)));  
+        intInput.setText(Integer.toString((int)Math.ceil(Math.random()*10)));
         lucInput.setText(Integer.toString((int)Math.ceil(Math.random()*10)));
 
     }
 
     Connection conn = null;
     void createConnection(){
-        try
-    {
-      // create a database connection
-      conn = DriverManager.getConnection("jdbc:sqlite:Fallout.db");
-      Statement st = conn.createStatement();
-      st.setQueryTimeout(30);  // set timeout to 30 sec.
-    }
+        try {
+            // create a database connection
+            conn = DriverManager.getConnection("jdbc:sqlite:Fallout.db");
+            Statement st = conn.createStatement();
+            st.setQueryTimeout(30);  // set timeout to 30 sec.
+        }
         catch(Exception e){
             e.printStackTrace();
             
         }
     }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    private void displayCharPerks(ArrayList<String> perkNames, ArrayList<String> perkDescs) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //TODO: this is where someone is supposed to take the lists I had from the query then put them in the correct tab.
+    }
 }
